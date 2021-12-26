a9d5ef4f95bb62827652b1d58d99bebd6ae70c3c
Cordell <cordellbonnieux@gmail.com>
2021-12-25 17:43:19 -0800

    Added rgba layer to the header bg, looking pretty tasty right now, moving on to the footer and content areas.

--- Header.jsx
+++ Header.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 function Header() {
   return (
     <div id="header">
-      <h1 className="siteTitle font_glass">Have A Good Laugh</h1>
+      <div className="header_inner">
+        <h1 className="header_title font_glass">Have A Good Laugh</h1>
+        <h2 className="header_tagline font_glass">Vancouver's Punk Fest</h2>
+      </div>
     </div>
   );
 }
